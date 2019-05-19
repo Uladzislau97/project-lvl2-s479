22acbc48715f9c7ea06d17e3cab58f5646434f41
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-05-19 13:25:39 +0300

    refactor(tests): send full paths to gendiff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,9 +5,8 @@ import gendiff from '../src';
 test('gendiff', () => {
   const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
   const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
-  const actualDiff = gendiff(
-    '__fixtures__/before_1.json',
-    '__fixtures__/after_1.json',
-  );
+  const beforeFullPath = path.resolve(__dirname, '__fixtures__/before_1.json');
+  const afterFullPath = path.resolve(__dirname, '__fixtures__/after_1.json');
+  const actualDiff = gendiff(beforeFullPath, afterFullPath);
   expect(actualDiff).toBe(expectedDiff);
 });
