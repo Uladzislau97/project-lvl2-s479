228a8181c74d61af9b1b5b6a704c63cfee30ed10
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-05-20 08:30:11 +0300

    feat(gendiff): add support of ini files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,20 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-test('gendiff for JSON files', () => {
-  const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
-  const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
-  const beforeFullPath = path.resolve(__dirname, '__fixtures__/before_1.json');
-  const afterFullPath = path.resolve(__dirname, '__fixtures__/after_1.json');
-  const actualDiff = gendiff(beforeFullPath, afterFullPath);
-  expect(actualDiff).toBe(expectedDiff);
-});
+const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
+const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+const filesPaths = [
+  ['__fixtures__/before_1.json', '__fixtures__/after_1.json'],
+  ['__fixtures__/before_2.yml', '__fixtures__/after_2.yml'],
+  ['__fixtures__/before_3.ini', '__fixtures__/after_3.ini'],
+];
 
-test('gendiff for yaml files', () => {
-  const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
-  const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
-  const beforeFullPath = path.resolve(__dirname, '__fixtures__/before_2.yml');
-  const afterFullPath = path.resolve(__dirname, '__fixtures__/after_2.yml');
-  const actualDiff = gendiff(beforeFullPath, afterFullPath);
-  expect(actualDiff).toBe(expectedDiff);
-});
+test.each(filesPaths)(
+  'gendiff for different file types',
+  (beforePath, afterPath) => {
+    const beforeFullPath = path.resolve(__dirname, beforePath);
+    const afterFullPath = path.resolve(__dirname, afterPath);
+    const actualDiff = gendiff(beforeFullPath, afterFullPath);
+    expect(actualDiff).toBe(expectedDiff);
+  },
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,12 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parse = (content, extname) => {
   if (extname === '.yml') {
     return yaml.safeLoad(content);
+  }
+  if (extname === '.ini') {
+    return ini.parse(content);
   }
   return JSON.parse(content);
 };
