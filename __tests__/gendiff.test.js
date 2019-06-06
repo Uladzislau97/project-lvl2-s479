3cf26f53ec87e1275d8559b86eaf88602ca13090
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-06-07 00:10:07 +0300

    feat(tests): add tests for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,42 +2,88 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-describe('data without nestings', () => {
-  const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
-  const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
-  const filesPaths = [
-    ['__fixtures__/before_1.json', '__fixtures__/after_1.json'],
-    ['__fixtures__/before_2.yml', '__fixtures__/after_2.yml'],
-    ['__fixtures__/before_3.ini', '__fixtures__/after_3.ini'],
-  ];
+describe('nested format', () => {
+  describe('data without nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_1');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_1.json', '__fixtures__/after_1.json'],
+      ['__fixtures__/before_2.yml', '__fixtures__/after_2.yml'],
+      ['__fixtures__/before_3.ini', '__fixtures__/after_3.ini'],
+    ];
 
-  test.each(filesPaths)(
-    'return correct diff for different file types',
-    (beforePath, afterPath) => {
-      const beforeFullPath = path.resolve(__dirname, beforePath);
-      const afterFullPath = path.resolve(__dirname, afterPath);
-      const actualDiff = gendiff(beforeFullPath, afterFullPath);
-      expect(actualDiff).toBe(expectedDiff);
-    },
-  );
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
+
+  describe('data with nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_2');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_4.json', '__fixtures__/after_4.json'],
+      ['__fixtures__/before_5.yml', '__fixtures__/after_5.yml'],
+      ['__fixtures__/before_6.ini', '__fixtures__/after_6.ini'],
+    ];
+
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
 });
 
-describe('data with nestings', () => {
-  const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_2');
-  const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
-  const filesPaths = [
-    ['__fixtures__/before_4.json', '__fixtures__/after_4.json'],
-    ['__fixtures__/before_5.yml', '__fixtures__/after_5.yml'],
-    ['__fixtures__/before_6.ini', '__fixtures__/after_6.ini'],
-  ];
+describe('plain format', () => {
+  const format = 'plain';
 
-  test.each(filesPaths)(
-    'return correct diff for different file types',
-    (beforePath, afterPath) => {
-      const beforeFullPath = path.resolve(__dirname, beforePath);
-      const afterFullPath = path.resolve(__dirname, afterPath);
-      const actualDiff = gendiff(beforeFullPath, afterFullPath);
-      expect(actualDiff).toBe(expectedDiff);
-    },
-  );
+  describe('data without nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_3');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_1.json', '__fixtures__/after_1.json'],
+      ['__fixtures__/before_2.yml', '__fixtures__/after_2.yml'],
+      ['__fixtures__/before_3.ini', '__fixtures__/after_3.ini'],
+    ];
+
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath, format);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
+
+  describe('data with nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_4');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_4.json', '__fixtures__/after_4.json'],
+      ['__fixtures__/before_5.yml', '__fixtures__/after_5.yml'],
+      ['__fixtures__/before_6.ini', '__fixtures__/after_6.ini'],
+    ];
+
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath, format);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
 });
