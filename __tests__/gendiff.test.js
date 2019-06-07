3df5a8d57a8f0d4fd96148fd8270d8a0f40063c8
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-06-07 16:05:31 +0300

    feat(tests): add tests for json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -87,3 +87,47 @@ describe('plain format', () => {
     );
   });
 });
+
+describe('json format', () => {
+  const format = 'json';
+
+  describe('data without nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_5');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_1.json', '__fixtures__/after_1.json'],
+      ['__fixtures__/before_2.yml', '__fixtures__/after_2.yml'],
+      ['__fixtures__/before_3.ini', '__fixtures__/after_3.ini'],
+    ];
+
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath, format);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
+
+  describe('data with nestings', () => {
+    const diffFullPath = path.resolve(__dirname, '__fixtures__/diff_6');
+    const expectedDiff = fs.readFileSync(diffFullPath, 'utf8');
+    const filesPaths = [
+      ['__fixtures__/before_4.json', '__fixtures__/after_4.json'],
+      ['__fixtures__/before_5.yml', '__fixtures__/after_5.yml'],
+      ['__fixtures__/before_6.ini', '__fixtures__/after_6.ini'],
+    ];
+
+    test.each(filesPaths)(
+      'return correct diff for different file types',
+      (beforePath, afterPath) => {
+        const beforeFullPath = path.resolve(__dirname, beforePath);
+        const afterFullPath = path.resolve(__dirname, afterPath);
+        const actualDiff = gendiff(beforeFullPath, afterFullPath, format);
+        expect(actualDiff).toBe(expectedDiff);
+      },
+    );
+  });
+});
