fc778fd8c10e1516d170cf9159c13835ac556e1e
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-06-19 00:31:14 +0300

    refactor(formatters): replace if-else statements with switch

--- src/formatters/nested.js
+++ src/formatters/nested.js
@@ -14,46 +14,49 @@ const stringify = (data, indentSize) => {
   if (!(data instanceof Object)) {
     return data;
   }
-  const properties = Object.keys(data)
-    .map((key) => {
-      const value = stringify(data[key], indentSize + bigIndentSize);
-      const indentation = indent.repeat(indentSize + bigIndentSize);
-      return `${indentation}${key}: ${value}`;
-    });
+  const properties = Object.keys(data).map((key) => {
+    const value = stringify(data[key], indentSize + bigIndentSize);
+    const indentation = indent.repeat(indentSize + bigIndentSize);
+    return `${indentation}${key}: ${value}`;
+  });
   return stringifyObjectByProperties(properties, indentSize);
 };
 
 const renderIter = (data, indentSize) => {
-  if (data.type === NodeTypes.object) {
-    const properties = data.properties.map(
-      prop => renderIter(prop, indentSize + smallIndentSize),
-    ).join('\n');
-    return stringifyObjectByProperties(properties, indentSize);
+  switch (data.type) {
+    case NodeTypes.object: {
+      const properties = data.properties.map(
+        prop => renderIter(prop, indentSize + smallIndentSize),
+      ).join('\n');
+      return stringifyObjectByProperties(properties, indentSize);
+    }
+    case NodeTypes.complex: {
+      const value = renderIter(data.value, indentSize + smallIndentSize);
+      const indentation = indent.repeat(indentSize);
+      return `${indentation}  ${data.key}: ${value}`;
+    }
+    case NodeTypes.unchanged: {
+      const value = stringify(data.value, indentSize + smallIndentSize);
+      const indentation = indent.repeat(indentSize);
+      return `${indentation}  ${data.key}: ${value}`;
+    }
+    case NodeTypes.removed: {
+      const value = stringify(data.value, indentSize + smallIndentSize);
+      const indentation = indent.repeat(indentSize);
+      return `${indentation}- ${data.key}: ${value}`;
+    }
+    case NodeTypes.added: {
+      const value = stringify(data.value, indentSize + smallIndentSize);
+      const indentation = indent.repeat(indentSize);
+      return `${indentation}+ ${data.key}: ${value}`;
+    }
+    default: {
+      const oldValue = stringify(data.value.old, indentSize + smallIndentSize);
+      const newValue = stringify(data.value.new, indentSize + smallIndentSize);
+      const indentation = indent.repeat(indentSize);
+      return `${indentation}- ${data.key}: ${oldValue}\n${indentation}+ ${data.key}: ${newValue}`;
+    }
   }
-  if (data.type === NodeTypes.complex) {
-    const value = renderIter(data.value, indentSize + smallIndentSize);
-    const indentation = indent.repeat(indentSize);
-    return `${indentation}  ${data.key}: ${value}`;
-  }
-  if (data.type === NodeTypes.unchanged) {
-    const value = stringify(data.value, indentSize + smallIndentSize);
-    const indentation = indent.repeat(indentSize);
-    return `${indentation}  ${data.key}: ${value}`;
-  }
-  if (data.type === NodeTypes.removed) {
-    const value = stringify(data.value, indentSize + smallIndentSize);
-    const indentation = indent.repeat(indentSize);
-    return `${indentation}- ${data.key}: ${value}`;
-  }
-  if (data.type === NodeTypes.added) {
-    const value = stringify(data.value, indentSize + smallIndentSize);
-    const indentation = indent.repeat(indentSize);
-    return `${indentation}+ ${data.key}: ${value}`;
-  }
-  const oldValue = stringify(data.value.old, indentSize + smallIndentSize);
-  const newValue = stringify(data.value.new, indentSize + smallIndentSize);
-  const indentation = indent.repeat(indentSize);
-  return `${indentation}- ${data.key}: ${oldValue}\n${indentation}+ ${data.key}: ${newValue}`;
 };
 
 const renderInNestedFormat = data => renderIter(data, initialIndentSize);
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,30 +13,36 @@ const stringify = (data) => {
 };
 
 const renderIter = (data, parents) => {
-  if (data.type === NodeTypes.object) {
-    return data.properties
-      .filter(prop => prop.type !== NodeTypes.unchanged)
-      .map(prop => renderIter(prop, parents))
-      .join('\n');
+  switch (data.type) {
+    case NodeTypes.object: {
+      return data.properties
+        .filter(prop => prop.type !== NodeTypes.unchanged)
+        .map(prop => renderIter(prop, parents))
+        .join('\n');
+    }
+    case NodeTypes.complex: {
+      const newParents = [...parents, data.key];
+      return data.value.properties
+        .filter(prop => prop.type !== NodeTypes.unchanged)
+        .map(prop => renderIter(prop, newParents))
+        .join('\n');
+    }
+    case NodeTypes.added: {
+      const propertyName = [...parents, data.key].join('.');
+      const value = stringify(data.value);
+      return `Property '${propertyName}' was added with value: ${value}`;
+    }
+    case NodeTypes.removed: {
+      const propertyName = [...parents, data.key].join('.');
+      return `Property '${propertyName}' was removed`;
+    }
+    default: {
+      const propertyName = [...parents, data.key].join('.');
+      const oldValue = stringify(data.value.old);
+      const newValue = stringify(data.value.new);
+      return `Property '${propertyName}' was updated. From ${oldValue} to ${newValue}`;
+    }
   }
-  if (data.type === NodeTypes.complex) {
-    const newParents = [...parents, data.key];
-    return data.value.properties
-      .filter(prop => prop.type !== NodeTypes.unchanged)
-      .map(prop => renderIter(prop, newParents))
-      .join('\n');
-  }
-  const propertyName = [...parents, data.key].join('.');
-  if (data.type === NodeTypes.added) {
-    const value = stringify(data.value);
-    return `Property '${propertyName}' was added with value: ${value}`;
-  }
-  if (data.type === NodeTypes.removed) {
-    return `Property '${propertyName}' was removed`;
-  }
-  const oldValue = stringify(data.value.old);
-  const newValue = stringify(data.value.new);
-  return `Property '${propertyName}' was updated. From ${oldValue} to ${newValue}`;
 };
 
 const renderInPlainFormat = data => renderIter(data, initialParents);
