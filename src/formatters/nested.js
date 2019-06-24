79956fc039da0eb431e16fe1c108eb3bb70c06b0
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-06-24 22:21:36 +0300

    refactor(nested-formatter): build indentation based on depth

--- src/formatters/nested.js
+++ src/formatters/nested.js
@@ -1,65 +1,68 @@
 import NodeTypes from '../node-types';
 
-const indent = ' ';
-const initialIndentSize = 0;
-const smallIndentSize = 2;
-const bigIndentSize = 4;
+const indentLength = 4;
+const spaceForSignLength = 2;
 
-const stringifyObjectByProperties = (properties, indentSize) => {
-  const indentation = indent.repeat(indentSize);
-  return `{\n${properties}\n${indentation}}`;
-};
-
-const stringify = (data, indentSize) => {
+const stringify = (data, depth) => {
   if (!(data instanceof Object)) {
     return data;
   }
   const properties = Object.keys(data).map((key) => {
-    const value = stringify(data[key], indentSize + bigIndentSize);
-    const indentation = indent.repeat(indentSize + bigIndentSize);
+    const currentDepth = depth + 1;
+    const value = stringify(data[key], currentDepth);
+    const indentationLength = indentLength * currentDepth;
+    const indentation = ' '.repeat(indentationLength);
     return `${indentation}${key}: ${value}`;
   });
-  return stringifyObjectByProperties(properties, indentSize);
+  const indentation = ' '.repeat(indentLength * depth);
+  return `{\n${properties}\n${indentation}}`;
 };
 
-const renderIter = (data, indentSize) => {
+const renderIter = (data, depth = 0) => {
   switch (data.type) {
     case NodeTypes.object: {
       const properties = data.properties.map(
-        prop => renderIter(prop, indentSize + smallIndentSize),
+        prop => renderIter(prop, depth + 1),
       ).join('\n');
-      return stringifyObjectByProperties(properties, indentSize);
+      const indentationLength = indentLength * depth;
+      const indentation = ' '.repeat(indentationLength);
+      return `{\n${properties}\n${indentation}}`;
     }
     case NodeTypes.complex: {
-      const value = renderIter(data.value, indentSize + smallIndentSize);
-      const indentation = indent.repeat(indentSize);
-      return `${indentation}  ${data.key}: ${value}`;
+      const value = renderIter(data.value, depth);
+      const indentationLength = indentLength * depth;
+      const indentation = ' '.repeat(indentationLength);
+      return `${indentation}${data.key}: ${value}`;
     }
     case NodeTypes.unchanged: {
-      const value = stringify(data.value, indentSize + smallIndentSize);
-      const indentation = indent.repeat(indentSize);
-      return `${indentation}  ${data.key}: ${value}`;
+      const value = stringify(data.value, depth);
+      const indentationLength = indentLength * depth;
+      const indentation = ' '.repeat(indentationLength);
+      return `${indentation}${data.key}: ${value}`;
     }
     case NodeTypes.removed: {
-      const value = stringify(data.value, indentSize + smallIndentSize);
-      const indentation = indent.repeat(indentSize);
+      const value = stringify(data.value, depth);
+      const indentationLength = indentLength * depth - spaceForSignLength;
+      const indentation = ' '.repeat(indentationLength);
       return `${indentation}- ${data.key}: ${value}`;
     }
     case NodeTypes.added: {
-      const value = stringify(data.value, indentSize + smallIndentSize);
-      const indentation = indent.repeat(indentSize);
+      const value = stringify(data.value, depth);
+      const indentationLength = indentLength * depth - spaceForSignLength;
+      const indentation = ' '.repeat(indentationLength);
       return `${indentation}+ ${data.key}: ${value}`;
     }
     case NodeTypes.updated: {
-      const oldValue = stringify(data.value.old, indentSize + smallIndentSize);
-      const newValue = stringify(data.value.new, indentSize + smallIndentSize);
-      const indentation = indent.repeat(indentSize);
+      const oldValue = stringify(data.value.old, depth);
+      const newValue = stringify(data.value.new, depth);
+      const indentationLength = indentLength * depth - spaceForSignLength;
+      const indentation = ' '.repeat(indentationLength);
       return `${indentation}- ${data.key}: ${oldValue}\n${indentation}+ ${data.key}: ${newValue}`;
     }
     default: return '';
   }
 };
 
-const renderInNestedFormat = data => renderIter(data, initialIndentSize);
+const renderInNestedFormat = data => renderIter(data);
 
 export default renderInNestedFormat;
